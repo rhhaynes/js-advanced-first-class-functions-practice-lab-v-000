e3fec0ce98402a3f7e7ce3d71b1abbab4d0159e4
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 02:04:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function logDriverNames(arr){
+  arr.forEach(
+    function(obj){console.log(obj.name)}
+  );
+}
+
+function logDriversByHometown(arr, loc){
+  arr.forEach(
+    function(obj){
+      if (obj.hometown === loc){console.log(obj.name)}
+    }
+  );
+}
+
+function driversByRevenue(arr){
+  return arr.slice().sort(
+    function(a, b){return a.revenue - b.revenue}
+  );
+}
+
+function driversByName(arr){
+  return arr.slice().sort(
+    function(a, b){return a.name.localeCompare(b.name)}
+  );
+}
+
+function totalRevenue(arr){
+  return arr.reduce(
+    function(sum, obj){return sum + obj.revenue}, 0
+  );
+}
+
+function averageRevenue(arr){
+  return totalRevenue(arr) / arr.length;
+}
